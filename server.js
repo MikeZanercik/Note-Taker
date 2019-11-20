d6fba988e37af2357b205a43862283238c53da64
Mike Zanercik <Michael.zanercik@gmail.com>
2019-11-20 09:30:38 -0500

    moved server.js file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const http = require("http");
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
