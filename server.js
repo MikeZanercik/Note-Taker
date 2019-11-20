f490d4e54c240f7d26af7f524a85f4929d398afe
Mike Zanercik <Michael.zanercik@gmail.com>
2019-11-20 16:08:55 -0500

    saving

--- server.js
+++ server.js
@@ -11,34 +11,34 @@ app.use(express.static('public'))
 
 const db = require('./public/db.json')
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-
-app.get("/api/notes", function (req, res) {
-  return res.json(db)
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 
-app.post("/api/notes", function (req, res) {
-  const newNote = JSON.stringify(req.body);
-  console.log(newNote)
-  fs.appendFile(path.join(__dirname, "/public/db.json"), newNote, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
+app.get("/api/notes", function(req, res) {
+    return res.json(db)
+  });
+
+ 
+  app.post("/api/notes", function(req, res) {
+    const newNote = JSON.stringify(req.body);
+    console.log(newNote)
+    fs.appendFile(path.join(__dirname,"/public/db.json"), newNote, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    })
+    res.json(db);
+  });
+
+  app.delete("/api/notes/:id", function (req, res) {
+    res.send("Note Deleted")
   })
-  res.json(newNote);
-});
-
-app.delete("/api/notes/:id", function (req, res) {
-  res.send("Note Deleted")
-})
-
+  
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}/`)
+    console.log(`Server listening at http://localhost:${PORT}/`)
 })
