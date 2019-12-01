2ce042f9deadddf83457045d07e2d6e3a3d418d7
Mike Zanercik <Michael.zanercik@gmail.com>
2019-12-01 14:18:16 -0500

    pushing most recent changes

--- server.js
+++ server.js
@@ -10,37 +10,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-const db = require('./public/db.json')
-// const js = require("./public/index")
+const db = require('./db.json')
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 
-app.get("/api/notes", function(req, res) {
-    return res.json(db)
-  });
+app.get("/api/notes", (req, res) => {
+  return res.json(db)
+});
 
- 
-  app.post("/api/notes", function(req, res) {
-    const newNote = JSON.stringify(req.body);
-    console.log(newNote)
-    fs.appendFile(path.join(__dirname,"/public/db.json"), newNote, function (err) {
-      if (err) throw err;
-      console.log('Saved!');
-    })
-    res.json(db);
-  });
 
-  app.delete("/api/notes/:id", function (req, res) {
-    res.send("Note Deleted")
-  })
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  console.log(newNote)
+  res.json(db.push(newNote));
+  fs.appendFile(path.join(__dirname, "/db.json"), JSON.stringify(newNote),  (err) => {
+    if (err) throw err;
+    console.log('Saved!');
+    console.log(db)
+  });
+  res.json(db)
   
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.send("Note Deleted")
+})
+
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}/`)
+  console.log(`Server listening at http://localhost:${PORT}/`)
 })
